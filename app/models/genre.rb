d6b13976cc5aa9d69d7ad3dcf06719118415dd88
unknown <c13388871@BUS-1025-41.ict.ad.dit.ie>
2016-01-26 15:04:26 +0000

    Validation added

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ActiveRecord::Base
 	has_many :movies
+	
+	
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
 	belongs_to :genre, dependent: :destroy
+	
+	validates :title, :released_on, presence: true
+	
+	
+	
 end
